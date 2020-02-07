61e852d3cbf21c6f253f4f916a5d2504015265f9
Song <oos9596@gmail.com>
2020-02-07 16:17:41 +0900

    deploy

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/Butt3r/TIL.git master:gh-pages
+
+
+
+cd -
+
