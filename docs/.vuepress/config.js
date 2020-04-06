fdd2e42e5453e109807f93687a41e54fb7016a1f
Song <oos9596@gmail.com>
2020-04-06 17:56:40 +0900

    changed post name

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,7 +24,7 @@ module.exports = {
         collapsable: true,
         children: [
           ['JAVA/java-basic', 'About Java'],
-          ['JAVA/java-file-structure', '자바 기본 구조'],
+          ['JAVA/java-file-structure', 'Java File Structure'],
         ],
       },
       {
