683894885e97314bcfb0bb874cbc55e1f14eb2b2
Song <oos9596@gmail.com>
2020-04-06 17:51:27 +0900

    new post is ready

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,6 +24,7 @@ module.exports = {
         collapsable: true,
         children: [
           ['JAVA/java-basic', 'About Java'],
+          ['JAVA/java-file-structure', '자바 기본 구조'],
         ],
       },
       {
